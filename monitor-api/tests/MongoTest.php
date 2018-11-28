dee02404b15a9e0c0428aed8fc7b7959c7da9490
Ilya Goryachev <ilyatos.gor@gmail.com>
2018-11-28 23:57:42 +0300

    Move tests

--- monitor-api/tests/MongoTest.php
+++ monitor-api/tests/MongoTest.php
@@ -7,21 +7,22 @@ use PHPUnit\Framework\TestCase;
 
 class MongoTest extends TestCase
 {
+    public function testConnection()
+    {
+        $connection = Mongo::connection();
+        $connection->listDatabases();
+    }
+
+    public function testDatabase()
+    {
+        //TODO: write test case for Database connection
+    }
+
     public function testProperSingleton()
     {
         $a = Mongo::connection();
         $b = Mongo::connection();
 
         $this->assertTrue($a === $b);
-    }
-
-    public function testConnection()
-    {
-        //TODO: write test case for Mongo connection
-    }
-
-    public function testDatabase()
-    {
-        //TODO: write test case for Database connection
     }
 }
