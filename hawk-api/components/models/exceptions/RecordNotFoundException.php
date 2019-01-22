21e145fd5c52ae73281a033945eb66ee58eaef0d
Ilya Goryachev <ilyatos.gor@gmail.com>
2019-01-23 00:28:59 +0300

    Change RecordNotFoundException structure

--- hawk-api/components/models/exceptions/RecordNotFoundException.php
+++ hawk-api/components/models/exceptions/RecordNotFoundException.php
@@ -5,24 +5,21 @@ declare(strict_types=1);
 namespace App\Components\Models\Exceptions;
 
 /**
- * Class ModelException
+ * Class RecordNotFoundException
  *
- * @package App\Components\Base\Exceptions
+ * @package App\Components\Models\Exceptions
  */
 class RecordNotFoundException extends \Exception
 {
     /**
      * RecordNotFoundException constructor.
      *
-     * @param string          $collectionName
      * @param string          $message
      * @param int             $code
      * @param \Exception|null $previous
      */
-    public function __construct(string $collectionName, $message = 'Record not found.', $code = 0, \Exception $previous = null)
+    public function __construct(string $message, $code = 0, \Exception $previous = null)
     {
-        $message .= " Collection: $collectionName. ";
-
         parent::__construct($message, $code, $previous);
     }
 }
