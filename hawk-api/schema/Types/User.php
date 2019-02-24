e9be5cf53290f293bab921801d28875518cd2fef
Ilya Goryachev <ilyatos.gor@gmail.com>
2019-02-24 14:43:40 +0300

    Add ability to get projects and membership through user by filter

--- hawk-api/schema/Types/User.php
+++ hawk-api/schema/Types/User.php
@@ -9,6 +9,7 @@ use GraphQL\Type\Definition\{
     ObjectType,
     Type
 };
+use App\Components\Models\User as UserModel;
 
 /**
  * Class User
@@ -34,15 +35,30 @@ class User extends ObjectType
                         'type' => Type::string(),
                         'description' => 'Password'
                     ],
-//                    'projects' => [
-//                        'type' => Type::listOf(TypeRegistry::project()),
-//                        'description' => 'User\'s projects',
-//                        'resolve' => function ($root, $args) {
-//                            return [
-//                                //проекты пользователей
-//                            ];
-//                        }
-//                    ]
+                    'projects' => [
+                        'type' => Type::listOf(TypeRegistry::project()),
+                        'description' => 'User\'s projects',
+                        'args' => [
+                            '_id' => Type::id(),
+                            'token' => Type::string(),
+                            'name' => Type::string()
+                        ],
+                        'resolve' => function (UserModel $root, $args) {
+                            return $root->projects($args);
+                        }
+                    ],
+                    'membership' => [
+                        'type' => Type::listOf(TypeRegistry::membership()),
+                        'description' => 'User\'s membership',
+                        'args' => [
+                            '_id' => Type::id(),
+                            'projectId' => Type::string(),
+                            'projectUri' => Type::string()
+                        ],
+                        'resolve' => function (UserModel $root, $args) {
+                            return $root->membership($args);
+                        }
+                    ],
                 ];
             }
         ];
