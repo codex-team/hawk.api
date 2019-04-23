99df51d1f50a23de436e7bf4b7a86b7cb80e4952
Ilya Goryachev <ilyatos.gor@gmail.com>
2019-04-23 13:07:42 +0300

    update(Models/User): improve password generator

--- src/Components/Models/User.php
+++ src/Components/Models/User.php
@@ -44,9 +44,7 @@ final class User extends BaseModel
      */
     public static function generatePassword(int $length): string
     {
-        $chars = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcefghijklmnopqrstuvwxyz';
-
-        return substr(str_shuffle($chars), 0, $length);
+        return bin2hex(openssl_random_pseudo_bytes( $length / 2));
     }
 
     /**
